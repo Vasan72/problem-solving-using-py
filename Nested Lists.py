99f59925333e7113d5bc4a525856acc7565b1e8d
Vasanthakumar N <129778105+Vasan72@users.noreply.github.com>
2025-01-08 16:03:23 +0530

    Create Nested Lists.py

---
+++ Nested Lists.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    student = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        student.append([name, score])
+    grade = [item[1] for item in student]
+    
+    list_grade = sorted(set(grade))
+    second_score = list_grade[1]
+    
+    result = [item[0] for item in student if item[1] == second_score]
+    result.sort() 
+    
+    for name in result:
+        print(name)
