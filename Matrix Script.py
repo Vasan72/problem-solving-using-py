31fc85310c02b8df7118ed9cda2434e849ed7af2
Vasanthakumar N <129778105+Vasan72@users.noreply.github.com>
2025-01-29 14:17:48 +0530

    Create Matrix Script.py

---
+++ Matrix Script.py
@@ -0,0 +1,22 @@
+import re
+
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+m = int(first_multiple_input[1])
+
+matrix = []
+
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+    
+decoded_string = ""
+for col in range(m):
+    for row in range(n):
+        decoded_string += matrix[row][col]
+
+decoded_string = re.sub(r'(?<=\w)[^a-zA-Z0-9]+(?=\w)',' ', decoded_string)
+
+print(decoded_string)
