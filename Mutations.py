cc6c7dba3baf8af064b407f24ecf43d3986135ff
Vasanthakumar N <129778105+Vasan72@users.noreply.github.com>
2025-01-30 22:17:31 +0530

    Update Mutations.py

--- Mutations.py
+++ Mutations.py
@@ -3,7 +3,7 @@ def mutate_string(string, position, character):
     l[position] = character
     string = "".join(l)
     return string
-
+ 
 if __name__ == '__main__':
     s = input()
     i, c = input().split()
