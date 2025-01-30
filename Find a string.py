0f84e75c8bbb7a0e60bba1b01eccc9bec9c567bc
Vasanthakumar N <129778105+Vasan72@users.noreply.github.com>
2025-01-30 22:16:49 +0530

    Update Find a string.py

--- Find a string.py
+++ Find a string.py
@@ -3,7 +3,7 @@ def count_substring(string, sub_string):
     for i in range(len(string) - len(sub_string) + 1):
         if string[i:i + len(sub_string)] == sub_string:
             count +=1
-    return count
+    return count 
 
 if __name__ == '__main__':
     string = input().strip()
